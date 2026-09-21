bfb314175696522458504299fc95442073fcc6cd
DarkoRodriguez <dark.rodriguez@duocuc.cl>
2026-09-21 01:26:22 -0300

    fix: variables T

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://34.228.83.169/api',
   azure: {
     tenantId: '85285dad-32cb-460f-ac7f-a654027af079',
     clientId: '91450f63-2a70-428b-bb55-1aec937e0dc8',
     scope: 'api://91450f63-2a70-428b-bb55-1aec937e0dc8/access_as_user',
-    redirectUri: 'http://localhost:4200/',
-    postLogoutRedirectUri: 'http://localhost:4200/',
+    redirectUri: 'https://34.228.83.169/',
+    postLogoutRedirectUri: 'https://34.228.83.169/',
   },
 };
+
 
